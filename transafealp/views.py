4e54783869ee7aaac28b9fbe1bc0085c871eb533
ernestoarbitrio <ernesto.arbitrio@gmail.com>
2013-04-24 17:32:15 +0200

    Prova commit

--- transafealp/views.py
+++ transafealp/views.py
@@ -2,7 +2,6 @@
 __author__ = 'ernesto'
 
 #This file contains only the views for the main app. It's made just to render an home page for the project
-
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
