e76949f50b12f14d40a00aae4dcde87f25050af4
Ernesto Arbitrio <ernesto@MacBook-Pro-di-Ernesto-Arbitrio-4.local>
2013-04-24 17:24:10 +0200

    base files commit

---
+++ transafealp/urls.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import patterns, include, url
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+
+
+
+
+admin.autodiscover()
+
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'transafealp.views.home', name='home'),
+    # url(r'^transafealp/', include('transafealp.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+        (r'^admin/', include(admin.site.urls)),
+        (r'^$', 'transafealp.views.main_view'),
+        (r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'registration/login.html'}),
+        (r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/accounts/login'}),
+)
---
+++ transafealp/views.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+__author__ = 'ernesto'
+
+#This file contains only the views for the main app. It's made just to render an home page for the project
+
+from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+
+@login_required
+def main_view(request):
+    context = {}
+    return render_to_response('transafealp/home_page.html', context, context_instance=RequestContext(request))
+
+
