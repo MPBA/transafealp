0a07e43482b775b88a14572c5b8ab209d4198816
ernestoarbitrio <ernesto.arbitrio@gmail.com>
2013-05-14 17:04:03 +0200

    bug fixing into add actors to action. Jquery check if no actor checked

--- transafealp/urls.py
+++ transafealp/urls.py
@@ -12,20 +12,24 @@ from plrutils import urls
 admin.autodiscover()
 
 urlpatterns = patterns('',
+
+        #################### ADMIN APPS URLS #######################
         (r'^admin_tools/', include('admin_tools.urls')),
         (r'^admin/', include(admin.site.urls)),
         ('^autocomplete/', include(autocomplete.urls)),
         ('^pages/', include('django.contrib.flatpages.urls')),
 
-        #################### PLRUTILS #########################
+        #################### PLRUTILS URLS #########################
         (r'plr/', include(urls.urlpatterns)),
 
+        #################### MAIN VIEWS URLS AND AUTH URLS #########################
         (r'^$', 'transafealp.views.main_view'),
         (r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'registration/login.html'}),
         (r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/accounts/login'}),
 
         #################### SCENARIO URLS #########################
         (r'^scenario/',   include('scenario.urls')),
+
         #################### JITES URLS #########################
         (r'^jites/',   include('jites.urls')),
 
