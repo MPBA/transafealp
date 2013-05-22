3a31138591fbd9b34cb0f1ffcfbe09a821c7be2f
Gabriele Franch <franch@gmail.com>
2013-05-22 19:12:29 +0200

    initial work on start event function

--- sql/transafealp_api.sql
+++ sql/transafealp_api.sql
@@ -2,7 +2,7 @@
 create extension if not exists plr;
 
 --print a (not so pretty) scenario actions graph
-DROP FUNCTION IF EXISTS graph_action (bigint,int,int);
+DROP FUNCTION IF EXISTS graph_action (bigint,int,int) CASCADE;
 CREATE OR REPLACE FUNCTION graph_action (scenario_id bigint,width int, height int)
 RETURNS bytea AS
 $BODY$
@@ -37,3 +37,48 @@ select * from graph_action(
 -- since pl/r is an untrusted language pl/r functions need superuser
 -- permissions to be created. Later we assign grant to normal db user
 grant all on all functions in schema public to transafe_dev; 
+
+--starts a new event for a given scenario
+DROP FUNCTION IF EXISTS start_event (text,boolean,Geometry(Point,3035)) CASCADE;
+CREATE OR REPLACE FUNCTION start_event (scenario_name text, is_real boolean, event_geom Geometry(Point,3035))
+RETURNS event AS
+$BODY$
+DECLARE
+	ev event;
+	scen scenario;
+BEGIN
+	--let's create the new event!
+	SELECT INTO scen * FROM scenario WHERE name = scenario_name;
+	IF NOT FOUND THEN
+		RAISE EXCEPTION 'Scenario not found!';
+	END IF;
+	ev.managing_authority_id = scen.managing_authority_id;
+	ev.event_name = scen.name;
+	ev.event_description = scen.description;
+	ev.scenario_geom = scen.geom;
+	
+	SELECT INTO ev.category_name,ev.category_description,ev.subcategory_name,ev.subcategory_description
+		c.name,c.description,sc.name,sc.description
+		FROM scenario_subcategory sc LEFT JOIN scenario_category c ON sc.category_id = c.id
+		WHERE sc.id = scen.subcategory_id;
+
+	ev.id = nextval('event_id_seq');
+	ev.is_real = is_real;
+	ev.time_start = statement_timestamp();
+	ev.status = 'open'::TEXT;
+	ev.time_end = NULL;
+	ev.event_geom = event_geom;
+
+	INSERT INTO event VALUES (ev.*);
+	
+	
+	--copy all actions for the given scenario
+	INSERT INTO ev_action (event_id,name,numcode,description,duration,status,comment)
+		VALUES ((SELECT ev.id,name,numcode,description,duration FROM action WHERE scenario_id = scen.id));
+	
+	RETURN ev;
+END
+$BODY$
+LANGUAGE plpgsql;
+
+select * from start_event('Frejus [SECT1/2/A]',false,'SRID=3035;POINT(0 0)');
