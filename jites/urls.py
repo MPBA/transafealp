7017eecd77f896f58cf30fc3cee3a3f4b316ade8
ernestoarbitrio <ernesto.arbitrio@gmail.com>
2013-05-30 17:15:37 +0200

    nested dict from evactiongraph model

--- jites/urls.py
+++ jites/urls.py
@@ -12,6 +12,8 @@ urlpatterns = patterns('jites.views',
    (r'event/(?P<scenario_id>\d+)/(?P<type>\w+)/start$', 'start_event' ),
    (r'dashboard/(?P<displaymode>\w+)/(?P<event_id>\d+)$', 'dashboard' ),
    (r'get_event/(?P<pk>\d+)$', EventDetailView.as_view() ),
-   (r'event/(?P<event_id>\d+)/add/message/$', 'save_event_message')
+   (r'event/(?P<event_id>\d+)/add/message/$', 'save_event_message'),
+   (r'tree/to/json/(?P<event_id>\d+)/$', 'tree_to_json')
+
 
 )
