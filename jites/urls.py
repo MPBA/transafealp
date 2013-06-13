90f00f02491711db6200bd61606b1b60ef9cb0e4
ernestoarbitrio <ernesto.arbitrio@gmail.com>
2013-06-13 18:29:46 +0200

    event stats after closed himself

--- jites/urls.py
+++ jites/urls.py
@@ -17,7 +17,6 @@ urlpatterns = patterns('jites.views',
    (r'get_action/(?P<pk>\d+)$', ActionDetailView.as_view() ),
    (r'^rerouting/(?P<type>\w+)', 'run_rerouting'),
    (r'^proxy/', 'proxy'),
-
    (r'^event/list', 'eventlist'),
-
+   (r'^event/stats/(?P<event_id>\d+)/$', 'event_statistics'),
 )
